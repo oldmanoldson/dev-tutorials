290282c97603c81c3e82f386bffc85aad259f76c
Sean Olson <34286508+oldmanoldson@users.noreply.github.com>
2024-10-02 20:53:08 -0400

    init commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string firstName = "Bob";
+string greeting = "Hello";
+string message = $"{greeting} {firstName}!";
+Console.WriteLine(message);
